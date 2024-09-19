5e045b20bbd93b6d8fe0c9247078089e3ac258ad
patriciasilva <patriciasilva@deso-se.com.br>
2024-09-19 16:37:05 -0300

    alterações na imagem

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, Image, StyleSheet, View } from "react-native";
 import { StackTypes } from "../../routes/stack";
 import { useNavigation } from "@react-navigation/native";
 
@@ -7,7 +7,9 @@ export function Home() {
     const navigation = useNavigation<StackTypes>();
     return (
         <LinearGradient style={styles.container} colors={['#42A5EB', "#2B6EEB"]}>
-            <View style={{ borderRadius: 200, backgroundColor: "white", width: 300, height: 300, margin: 12 }}></View>
+            <View style={{ borderRadius: 200, backgroundColor: "white", width: 300, height: 300, margin: 12 }}>
+                <Image source={require("../../assets/terra.jpg")} style={{ borderRadius: 200, backgroundColor: "white", width: 300, height: 300 }}/>
+            </View>
             <Button
                 color={"blue"}
                 title={"AllCountries"}
